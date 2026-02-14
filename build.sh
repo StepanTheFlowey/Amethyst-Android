58fa3e80ff7b8d7089a76cd476e87b84a50f09b9
flowey-pc <stepanmasirov@gmail.com>
2026-02-14 13:17:36 +0500

    build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ git clean -xdf
 git submodule foreach git clean -xdf
 
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/
-export JAVA_TOOL_OPTIONS="-XX:+UnlockExperimentalVMOptions -XX:+AlwaysPreTouch -XX:-TieredCompilation -XX:CICompilerCount=$(nproc) -XX:ReservedCodeCacheSize=2G -XX:+DisableAttachMechanism -XX:-DontCompileHugeMethods -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UseFPUForSpilling -XX:+UseStringDeduplication -XX:+UseTransparentHugePages -XX:+UseXMMForArrayCopy -XX:+UseFastUnorderedTimeStamps"
-exec ./gradlew --no-daemon assembleRelease
+export JAVA_TOOL_OPTIONS="-Xmx4G -XX:CICompilerCount=$(nproc) -XX:ReservedCodeCacheSize=2G -XX:+DisableAttachMechanism -XX:-DontCompileHugeMethods -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UseFPUForSpilling -XX:+UseStringDeduplication -XX:+UseTransparentHugePages -XX:+UseXMMForArrayCopy"
+exec nice ./gradlew --no-daemon :app_pojavlauncher:assembleRelease
