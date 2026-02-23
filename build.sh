04852296b7bdfcb5967a1bb39bf7bf4c827c6182
flowey-pc <stepanmasirov@gmail.com>
2026-02-23 20:35:57 +0500

    build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ clear
 git clean -xdf
 git submodule foreach git clean -xdf
 
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 export JAVA_TOOL_OPTIONS="-Xmx4G -XX:CICompilerCount=$(nproc) -XX:ReservedCodeCacheSize=2G -XX:+DisableAttachMechanism -XX:-DontCompileHugeMethods -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UseFPUForSpilling -XX:+UseStringDeduplication -XX:+UseTransparentHugePages -XX:+UseXMMForArrayCopy"
 exec nice ./gradlew --no-daemon :app_pojavlauncher:assembleRelease
